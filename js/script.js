de10bc0f33ea43f59364ed5246a3d699a8ff3331
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2022-12-15 19:31:33 +0000

    imported repository to replit

--- js/script.js
+++ js/script.js
@@ -10,14 +10,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit6-03-JS/sw.js", {
+    scope: "/ICS2O-Unit6-03-JS/",
   })
 }
 
-/**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
-}
