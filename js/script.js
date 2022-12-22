5cb1b542c41b08373f7103d5f490f0a9df905bee
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2022-12-22 00:56:57 +0000

    borrowed and modified claire's code

--- js/script.js
+++ js/script.js
@@ -14,3 +14,41 @@ if (navigator.serviceWorker) {
     scope: "/ICS2O-Unit6-03-JS/",
   })
 }
+
+/**
+ * Get API info.
+ */
+// code from: https://www.youtube.com/watch?v=670f71LTWpM
+
+const getWeather = async (URLAddress) => {
+  try {
+    let tempInKelvin = 0
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData.weather[0].icon)
+    document.getElementById("api-image").innerHTML =
+      '<img src="http://openweathermap.org/img/wn/' +
+      jsonData.weather[0].icon +
+      '@2x.png" alt="API image" class="center" width= 20% height 20%' +
+      ">"
+
+    tempInKelvin = jsonData.main.temp
+    console.log(tempInKelvin)
+
+    let tempInCelsius = tempInKelvin - 273.15
+
+    if (jsonData.main.temp != "none") {
+      document.getElementById("temperature").innerHTML =
+        "<p>Temperature: " + tempInCelsius.toFixed(0) + "°C</p>"
+    } else {
+      document.getElementById("temperature").innerHTML =
+        "<p>Temperature: unknown</p>"
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+getWeather(
+  "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+)
